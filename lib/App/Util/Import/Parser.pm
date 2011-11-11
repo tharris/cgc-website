9631e6a83aea4447fe2325627f400d3fb2322938
Shiran Pasternak <shiranpasternak@gmail.com>
2011-11-11 00:12:20 -0500

    Parser for vertical (EGO) text (in strain import file) is now done. Moved TSV/EGO parsers to App::Util::Import::Parser, and added unit tests. Using the PerlUnit (Test::Unit) framework.

---
+++ lib/App/Util/Import/Parser.pm
@@ -0,0 +1,73 @@
+package App::Util::Import::Parser;
+
+use Exporter 'import';
+
+use strict;
+use warnings;
+
+our @EXPORT_OK = qw(curry_tsv_processor curry_ego_processor);
+
+=head2 curry_tsv_processor
+
+    Curry a TSV line processor
+
+=cut
+
+sub curry_tsv_processor {
+    my ($input, $index, $primary_idx) = @_;
+    return sub {
+        my $line    = shift;
+        my $fields  = [ split("\t", $line) ];
+        my $primary = $fields->[$primary_idx];
+        push @$input, $fields;
+        $index->{$primary} ||= [];
+        push @{ $index->{$primary} }, $fields;
+    };
+}
+
+=head2 curry_ego_processor
+
+    Curry a processor for a line from the vertical file format
+
+=cut
+
+sub curry_ego_processor {
+    my ($input, $index, $primary_idx) = @_;
+    my $is_header = 1;
+    my $field     = undef;
+    my @values   = ();
+    my $current_key       = undef;
+    my $item = {};
+    my $set_item_key = sub {
+        if (defined $current_key) {
+            $item->{$current_key} = join(' ', @values) || undef;
+            $current_key = undef;
+            @values = ();
+        }
+    };
+    return sub {
+        my $line = shift;
+        if ($is_header) {
+            if ($line =~ m/\s*\=+\s*$/) {
+                $is_header = 0;
+            }
+            return;
+        }
+        if ($line =~ m/^\s*\-+\s*/) { # Item delimiter
+            $set_item_key->();
+            push @$input, $item;
+            $item = {};
+            return;
+        }
+        my ($key, $value) = unpack('A11 x A*', $line); # Key: Value
+        $key =~ s/^\s+|\s+$//g;
+        $value =~ s/^\s+|\s+$//g;
+        if ($key ne '') {
+            $set_item_key->();
+            $current_key = $key;
+        }
+        push @values, $value;
+    };
+}
+
+1;
