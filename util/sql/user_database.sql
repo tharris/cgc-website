c6a1cf87dafcc2a6e60d3373a33abc93d1e18e2c
Shiran Pasternak <shiranpasternak@gmail.com>
2012-06-25 17:35:58 -0400

    Handling a few Wormbase-related errors.

--- util/sql/user_database.sql
+++ util/sql/user_database.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-            user_id            INTEGER AUTO_INCREMENT PRIMARY KEY, 
-            username      char(255),
+            user_id       INTEGER AUTO_INCREMENT PRIMARY KEY, 
+			username      char(255),
             password      char(255),
-            gtalk_key	  text,
+            // gtalk_key	  text,
             active        int(11),
             wbid          char(255),
             wb_link_confirm     BOOLEAN
