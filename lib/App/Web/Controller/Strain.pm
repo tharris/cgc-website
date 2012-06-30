25622fda20355c11f928402545397b76693d1e9f
Shiran Pasternak <shiranpasternak@gmail.com>
2012-06-29 22:34:58 -0400

    Strain page is up, with search typeahead (minus search results).

--- lib/App/Web/Controller/Strain.pm
+++ lib/App/Web/Controller/Strain.pm
@@ -2,7 +2,15 @@ package App::Web::Controller::Strain;
 use Moose;
 use namespace::autoclean;
 
-BEGIN {extends 'Catalyst::Controller'; }
+BEGIN { extends 'Catalyst::Controller::REST'; }
+
+__PACKAGE__->config(
+	default => 'application/json',
+	map => {
+		'text/html' => [ qw/View TT/ ],
+		'application/json' => 'JSON',
+	}
+);
 
 =head1 NAME
 
@@ -16,17 +24,34 @@ Catalyst Controller.
 
 =cut
 
+sub strain : Path('/strain') : ActionClass('REST') { }
 
-=head2 index
-
-=cut
-
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
-
-    $c->response->body('Matched App::Web::Controller::Strain in Strain.');
+sub strain_GET {
+	my ($self, $c, $stash_key) = @_;
+	
+	$c->stash->{template} = 'strain/index.tt2';
+	$self->status_ok(
+		$c,
+		entity => {
+			foo => $stash_key
+		}
+	);
 }
 
+sub list : Local : ActionClass('REST') { }
+
+sub list_GET {
+	my ($self, $c) = @_;
+
+	my $strains = [ $c->model('CGC::Strain')->get_column('name')->all() ];
+	$self->status_ok(
+		$c,
+		entity => $strains
+	);	
+}
+
+# sub strain : Path('/strain') : Args(2) : ActionClass('REST') {}
+# sub strain_GET { my ($self,$c,@args) = @_; if $args[0] eq 'list' {}....
 
 =head1 AUTHOR
 
